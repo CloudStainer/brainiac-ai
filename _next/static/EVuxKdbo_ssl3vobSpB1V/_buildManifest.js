04abf2936fc825dadbf8d2b6d2325aabe0bcddac
CloudStainer <cloudstainer@gmail.com>
2024-12-13 16:57:56 -0500

    initial commit

---
+++ _next/static/EVuxKdbo_ssl3vobSpB1V/_buildManifest.js
@@ -0,0 +1,93 @@
+(self.__BUILD_MANIFEST = (function (a, s, e, c) {
+  return {
+    __rewrites: { afterFiles: [], beforeFiles: [], fallback: [] },
+    "/": [
+      "static/chunks/cb355538-8f394de0524b7701.js",
+      a,
+      e,
+      "static/chunks/208-31ea3173a7bfdf39.js",
+      s,
+      c,
+      "static/chunks/pages/index-e7c589dd76a44c99.js",
+    ],
+    "/_error": ["static/chunks/pages/_error-77823ddac6993d35.js"],
+    "/ai-refund-policy": [
+      a,
+      s,
+      "static/chunks/pages/ai-refund-policy-1617fd2d31c4771b.js",
+    ],
+    "/disclaimer": [a, s, "static/chunks/pages/disclaimer-4f3f67642aa6457e.js"],
+    "/learn-more": [
+      a,
+      s,
+      c,
+      "static/chunks/pages/learn-more-bd0adfd575081cdf.js",
+    ],
+    "/privacy-policy": [
+      a,
+      s,
+      "static/chunks/pages/privacy-policy-1e37d9ab8fe096d6.js",
+    ],
+    "/q4-and-beyond": [
+      a,
+      s,
+      "static/chunks/pages/q4-and-beyond-8206af9e21796eef.js",
+    ],
+    "/seven-mental-frameworks": [
+      a,
+      s,
+      "static/chunks/pages/seven-mental-frameworks-4cf88206b9f95db8.js",
+    ],
+    "/sitemap.xml": ["static/chunks/pages/sitemap.xml-5fb8d1f1c3c8f8af.js"],
+    "/skai": [a, e, s, "static/chunks/pages/skai-50f59487245f78db.js"],
+    "/terms-and-conditions": [
+      a,
+      s,
+      "static/chunks/pages/terms-and-conditions-f1eeaa506a06a915.js",
+    ],
+    "/the-paradox-of-human-progress": [
+      a,
+      s,
+      "static/chunks/pages/the-paradox-of-human-progress-74929e47f6e724d4.js",
+    ],
+    "/what-are-ai-agents": [
+      a,
+      s,
+      "static/chunks/pages/what-are-ai-agents-748c7dfb4304c1cf.js",
+    ],
+    "/what-is-skillfulai": [
+      a,
+      s,
+      "static/chunks/pages/what-is-skillfulai-c3fe39fbf91fa606.js",
+    ],
+    "/when-ai-learns-to-think-beyond-data-and-code": [
+      a,
+      s,
+      "static/chunks/pages/when-ai-learns-to-think-beyond-data-and-code-86d9a3a1f3e135cc.js",
+    ],
+    sortedPages: [
+      "/",
+      "/_app",
+      "/_error",
+      "/ai-refund-policy",
+      "/disclaimer",
+      "/learn-more",
+      "/privacy-policy",
+      "/q4-and-beyond",
+      "/seven-mental-frameworks",
+      "/sitemap.xml",
+      "/skai",
+      "/terms-and-conditions",
+      "/the-paradox-of-human-progress",
+      "/what-are-ai-agents",
+      "/what-is-skillfulai",
+      "/when-ai-learns-to-think-beyond-data-and-code",
+    ],
+  };
+})(
+  "static/chunks/403-ac3ea3c5fcae2fec.js",
+  "static/chunks/85-33d8c7e03382bc46.js",
+  "static/chunks/332-2d0cb4416365c819.js",
+  "static/chunks/961-ac11156e8e6c646e.js"
+)),
+  self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
